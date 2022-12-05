5cac07d716cfa54e2e072978a45a3a90f758960d
ledinh <lenguyennhutdinh@gmail.com>
2022-12-05 20:56:39 +0700

    change function solve equation

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 let calculateBtn = document.querySelector(".calculate-btn")
-let errorMessage = document.querySelector(".answer")
+let answer = document.querySelector(".answer")
 let clearBtn = document.querySelector(".clear-btn")
 let a, b, c
 
@@ -11,27 +11,34 @@ function solveQuadraticEquation(a, b, c) {
 	let delta = b * b - 4 * a * c
 	// Phuong trinh co vo so nghiem
 	if (b == 0 && c == 0) {
-		console.log("Vo so nghiem")
+		answer.innerHTML = "Phương trình có vô số nghiệm"
 	} else {
 		if (a == 0) {
 			// Giai phuong trinh bac 1 (a==0)
 			if (b == 0 && c != 0) {
-				console.log("Vo nghiem")
+				answer.innerHTML = "Phương trình vô nghiệm"
 			} else {
 				x = -c / b
-				console.log(x.toFixed(2))
+				answer.innerHTML =
+					"Phương trình có 1 nghiệm:<br>" + "x = " + x.toFixed(2)
 			}
 		} else {
 			// Giai phuong trinh bac 2 (a!=0)
 			if (delta < 0) {
-				console.log("Vo nghiem")
+				answer.innerHTML = "Phương trình vô nghiệm"
 			} else if (delta == 0) {
 				x = -b / (2 * a)
-				console.log(x.toFixed(2))
+				answer.innerHTML =
+					"Phương trình có 1 nghiệm kép:<br>" + "x = " + x.toFixed(2)
 			} else {
-				x1 = (-b - sqrt(delta)) / (2 * a)
-				x2 = (-b + sqrt(delta)) / (2 * a)
-				console.log(x1.toFixed(2), x2.toFixed(2))
+				x1 = (-b - Math.sqrt(delta)) / (2 * a)
+				x2 = (-b + Math.sqrt(delta)) / (2 * a)
+				answer.innerHTML =
+					"Phương trình có 2 nghiệm:" +
+					"<br>x<sub>1</sub> = " +
+					x1.toFixed(2) +
+					"<br>x<sub>2</sub> = " +
+					x2.toFixed(2)
 			}
 		}
 	}
@@ -79,6 +86,6 @@ calculateBtn.onclick = function () {
 	} else if (isNotNumber) {
 		alert(txt2)
 	} else {
-		solveQuadraticEquation(a,b,c)
+		solveQuadraticEquation(a, b, c)
 	}
 }
